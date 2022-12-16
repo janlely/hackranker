e82b765def4be016d286e978ecceb51c6dfeb847
Jay <janlely@163.com>
2022-12-16 10:23:34 +0800

    update

--- dice-path/app/Main.hs
+++ dice-path/app/Main.hs
@@ -2,3 +2,14 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+
+data Dice = Dice
+  { _top :: Int,
+  , _bottom :: Int
+  , _front :: Int,
+  , _back :: Int
+  , _left :: Int
+  , _right :: Int
+  } deriving (Show)
+
